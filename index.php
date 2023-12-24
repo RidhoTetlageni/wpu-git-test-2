89efe065e2100eb5df7b7ed11aab321390b1b558
RidhoTetlageni <ridhotetla2401@gmail.com>
2023-12-25 02:15:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
   <body>
     <h1>Hello Word!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt animi ratione impedit, explicabo facilis tenetur est porro iure. Suscipit nemo amet itaque fugit tempore beatae omnis, officia rerum doloremque. Aperiam!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
